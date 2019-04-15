588460313ec1aca6735a05073aa6574450d1c782
manojpanta <manojpanta95@gmail.com>
2019-04-14 20:14:32 -0600

    api end point forecast test

---
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+describe 'request to /api/v1/forecast with' do
+  it 'address params returns forcast for the location' do
+    location = "Denver"
+
+    get "/api/v1/forecast?location=#{location}"
+
+    result = JSON.parse(response.body)
+
+
+    expect(response).to be_successful
+    expect(result).to have_key("data")
+  end
+end
