9f6537d29b2fda5872de7b700359c165c7c11ab4
manojpanta <manojpanta95@gmail.com>
2019-04-14 20:15:06 -0600

    test for forecast service

---
+++ spec/services/forecast_service_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+describe ForecastService do
+  it 'exists' do
+    service = ForecastService.new
+    expect(service).to be_a(ForecastService)
+  end
+  it '#get_forecast method returns forecast for an address' do
+    service = ForecastService.new
+    lat_lon = {:lat => 39.7392358, :lng => -104.990251}
+
+    expect(service.get_forecast(lat_lon)).to be_a(Hash)
+  end
+  #
+  # it '#get_json method returns json object which contains many key value pairs' do
+  #   service = ForecastService.new
+  #   response = service.get_json("Denver")
+  #
+  #   expect(response).to be_a(Hash)
+  #   expect(response).to have_key(:lat)
+  #   expect(response).to have_key(:lng)
+  # end
+end
