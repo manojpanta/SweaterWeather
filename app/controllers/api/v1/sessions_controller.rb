e825eaad2e2dcf1e33dd151749eb42950c8c626e
manojpanta <manojpanta95@gmail.com>
2019-04-15 17:24:49 -0600

    user login with email and password body is working now

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user_info = JSON.parse(request.body.read)
+    user = User.find_by(email: user_info["email"])
+    if user && user.authenticate(user_info["password"])
+      session[:user_id] = user.id
+      render json: {
+        api_key: user.api_key
+      }, status: 200
+    else
+      render json: {
+        api_key: user.errors
+      }, status: 400
+    end
+  end
+end
