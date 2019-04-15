115391391c2df5b6059cd2f62494a33710c73310
manojpanta <manojpanta95@gmail.com>
2019-04-14 20:11:03 -0600

    added forecast facade that forecast controller calls

---
+++ app/facades/forecast_facade.rb
@@ -0,0 +1,29 @@
+class ForecastFacade
+  attr_reader :location, :id
+
+  def initialize(location)
+    @location = location
+    @id = 1
+  end
+
+  def current_temperature
+    service_response[:currently][:temperature]
+  end
+
+  def forecast
+    Forecast.new(service_response)
+  end
+
+  def service_response
+    @lat_lon ||= lat_lon_service.get_lat_lon(@location)
+    @response ||= forecast_service.get_forecast(@lat_lon)
+  end
+
+  def lat_lon_service
+    LatLonService.new
+  end
+
+  def forecast_service
+    ForecastService.new
+  end
+end
