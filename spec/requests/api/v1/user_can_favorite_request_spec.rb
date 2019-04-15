0893a984886b45b3c7cd7a875cac634ca307d696
manojpanta <manojpanta95@gmail.com>
2019-04-15 17:34:38 -0600

    added test for favoriting location

---
+++ spec/requests/api/v1/user_can_favorite_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+describe 'post request to api/v1/favorites', :type => :request do
+  it 'with a body having api_key and location name can favorite places' do
+    WebMock.disable!
+    user = User.create(email: 'manoj', password: 'this', api_key:'abchb')
+
+    body = { "location": "Denver",
+             "api_key": "abchb"
+            }
+
+    post '/api/v1/favorites', params: body.to_json, headers: { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+
+    result = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    expect(result).to be_a(Hash)
+  end
+end
