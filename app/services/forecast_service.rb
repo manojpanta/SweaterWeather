ead043f5fde8bcd15e9b9eb2a0301d1cf10cfc86
manojpanta <manojpanta95@gmail.com>
2019-04-14 20:13:09 -0600

    forecast service that gets forecast info from darksky api

---
+++ app/services/forecast_service.rb
@@ -0,0 +1,14 @@
+class ForecastService
+  def get_forecast(lat_lon)
+    get_json(lat_lon[:lat], lat_lon[:lng])
+  end
+
+  def get_json(lat,lon)
+    response = conn(lat,lon)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn(lat, lon)
+    Faraday.get("https://api.darksky.net/forecast/#{ENV["DARK-SKY-API-KEY"]}/#{lat},#{lon}")
+  end
+end
