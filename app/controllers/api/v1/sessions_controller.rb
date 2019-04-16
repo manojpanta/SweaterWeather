89025f092893e41afaeefe9e34f1e7cb9c3d9a60
manojpanta <manojpanta95@gmail.com>
2019-04-15 20:11:24 -0600

    edge case tested for user login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,8 +5,10 @@ class Api::V1::SessionsController < ApplicationController
     if user && user.authenticate(user_info["password"])
       session[:user_id] = user.id
       render json: {api_key: user.api_key}, status: 200
-    else
-      render json: { api_key: user.errors}, status: 400
+    elsif !user
+      render json: {error: 'Not a valid email'}, status: 401
+    elsif  !user.authenticate(user_info["password"])
+      render json: {error: 'Could not be authenticated!!'}, status: 401
     end
   end
 end
--- spec/requests/api/v1/user_can_login_spec.rb
+++ spec/requests/api/v1/user_can_login_spec.rb
@@ -19,4 +19,42 @@ describe 'post request to api/v1/sessions', :type => :request do
     expect(result).to be_a(Hash)
     expect(result).to have_key("api_key")
   end
+
+  it 'with a body having incorrect email can not log in' do
+    WebMock.disable!
+    User.create(email: 'whatever@example.com',
+                password: "password",
+                api_key: "a1234b")
+
+    user_info = { "email": "whatever11@example.com",##incorrect
+                  "password": "password",
+                }
+    post '/api/v1/sessions', params: user_info.to_json, headers: {
+    'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'
+    }
+
+    result = JSON.parse(response.body)
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+    expect(result["error"]).to eq("Not a valid email")
+  end
+
+  it 'with a body having incorrect password can not log in' do
+    WebMock.disable!
+    User.create(email: 'whatever@example.com',
+                password: "password",
+                api_key: "a1234b")
+
+    user_info = { "email": "whatever@example.com",##incorrect
+                  "password": "password123",
+                }
+    post '/api/v1/sessions', params: user_info.to_json, headers: {
+    'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'
+    }
+
+    result = JSON.parse(response.body)
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+    expect(result["error"]).to eq("Could not be authenticated!!")
+  end
 end
