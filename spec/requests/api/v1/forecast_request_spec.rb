00fc80d32dc5a0bd74a48c90f206407caa3cca5c
manojpanta <manojpanta95@gmail.com>
2019-04-16 18:55:18 -0600

    added test for forecast request spec

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -8,5 +8,31 @@ describe 'request to /api/v1/forecast with' do
 
     expect(response).to be_successful
     expect(result).to have_key("data")
+    expect(result["data"]["attributes"]).to have_key("location")
+    expect(result["data"]["attributes"]).to have_key("current_temperature")
+    expect(result["data"]["attributes"]).to have_key("time_now")
+    expect(result["data"]["attributes"]).to have_key("date_today")
+    expect(result["data"]["attributes"]).to have_key("highest_for_today")
+    expect(result["data"]["attributes"]).to have_key("lowest_for_today")
+    expect(result["data"]["attributes"]).to have_key("humidity")
+    expect(result["data"]["attributes"]).to have_key("visibility")
+    expect(result["data"]["attributes"]).to have_key("uvindex")
+    expect(result["data"]["attributes"]).to have_key("summary")
+    expect(result["data"]["attributes"]).to have_key("weekly_summary")
+
+    expect(result["data"]["attributes"]).to have_key("daily_forecast")
+    expect(result["data"]["attributes"]["daily_forecast"].first).to have_key("id")
+    expect(result["data"]["attributes"]["daily_forecast"].first).to have_key("date")
+    expect(result["data"]["attributes"]["daily_forecast"].first).to have_key("summary")
+    expect(result["data"]["attributes"]["daily_forecast"].first).to have_key("icon")
+    expect(result["data"]["attributes"]["daily_forecast"].first).to have_key("high")
+    expect(result["data"]["attributes"]["daily_forecast"].first).to have_key("low")
+    expect(result["data"]["attributes"]["daily_forecast"].first).to have_key("humidity")
+
+    expect(result["data"]["attributes"]).to have_key("hourly_forecast")
+    expect(result["data"]["attributes"]["hourly_forecast"].first).to have_key("id")
+    expect(result["data"]["attributes"]["hourly_forecast"].first).to have_key("time")
+    expect(result["data"]["attributes"]["hourly_forecast"].first).to have_key("icon")
+    expect(result["data"]["attributes"]["hourly_forecast"].first).to have_key("temperature")
   end
 end
